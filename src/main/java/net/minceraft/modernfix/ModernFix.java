03387f758563c6f25b884c903f2d1b3e31b6b92c
asdasda3456 <contact.minceraftyt@gmail.com>
2024-08-27 12:36:06 +0300

    very important changes

--- src/main/java/net/minceraft/modernfix/ModernFix.java
+++ src/main/java/net/minceraft/modernfix/ModernFix.java
@@ -15,6 +15,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+//asdasdasda
 // Very important Comment
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ModernFix.MOD_ID)
